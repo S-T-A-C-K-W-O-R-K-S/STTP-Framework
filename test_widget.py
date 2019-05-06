139b5276661695685e65a8cd4bb0cab2025bc6a8
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2019-05-06 17:01:28 +0100

    Create PYTEST.INI

--- test_widget.py
+++ test_widget.py
@@ -3,3 +3,6 @@ def test_widget_functions_as_expected():
 
 def test_widget_fails():
     assert False
+
+def some_convenience_function():
+    print("This will be ignored by PYTEST because the function name does not begin with 'test'.")
