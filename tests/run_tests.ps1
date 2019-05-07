819324dcaa63369d2864e549b61cd385e9b71a8f
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2019-05-07 17:37:19 +0100

    Update RUN_TESTS.PS1

--- tests/run_tests.ps1
+++ tests/run_tests.ps1
@@ -1,1 +1,1 @@
-pytest --env dev --html="test_results\results.html" --junitxml="test_results\results.xml" -m "not debug" -v -rxXs
+pytest --env dev -m "not debug" -v -rxs --html="test_results\results.html" --junitxml="test_results\results.xml"
