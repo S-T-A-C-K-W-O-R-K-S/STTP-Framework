6c20be785566041dd2f3779a61aa46f1e65fc6dc
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2019-06-02 21:00:12 +0100

    Add Test Data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,23 @@
+import json 
+
 from pytest import fixture
 from config import Config
 from selenium import webdriver
 
 
+data_path = '../test_data/meddbase.json'
+
+def load_test_data(path):
+    with open(path) as data_file:
+        data = json.load(data_file)
+        return data
+
 def pytest_addoption(parser):
     parser.addoption("--env", action="store", help="environment to run tests against")
-
 
 @fixture(scope="session")
 def env(request):
     return request.config.getoption("--env")
-
 
 @fixture(scope="session")
 def custom_config(env):
@@ -23,3 +30,8 @@ def browser(request):
     driver_instance = driver()
     yield driver_instance
     driver_instance.quit()
+
+@fixture(params=load_test_data(data_path))
+def test_data(request):
+    data = request.param
+    return data
